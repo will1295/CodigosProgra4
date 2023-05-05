660952a9252a25b0e8ed6d3e3abdf3dae3f8b469
Willian Montes <willian1295@hotmail.es>
2023-05-05 17:46:01 -0600

    Delete storage directory

--- storage/framework/views/21f13fa53aa913c9535dcf35d8aef5a7.php
+++
@@ -1,6 +0,0 @@
-
-
-<?php $__env->startSection('contenido'); ?>
-<button class="btn btn-danger">Click</button>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\practica\practica\resources\views/index.blade.php ENDPATH**/ ?>
